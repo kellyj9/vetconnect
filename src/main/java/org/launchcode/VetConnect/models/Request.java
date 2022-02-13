0bd4e6ea4ceed0332111844c90f8d89dedc99f52
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-02-13 09:48:41 -0500

    created request model class

---
+++ src/main/java/org/launchcode/VetConnect/models/Request.java
@@ -0,0 +1,163 @@
+package org.launchcode.VetConnect.models;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.NumberFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+public class Request extends AbstractEntity {
+
+    @NotBlank(message = "Name required")
+    private String name;
+
+    @NotBlank(message = "Address required")
+    private String address;
+
+    @NotBlank(message = "City required")
+    private String city;
+
+    @NotBlank(message = "State required")
+    private String state;
+
+    @NotBlank(message = "Zip required")
+    @NumberFormat
+    private String zip;
+
+    @NotBlank(message = "Phone Number required")
+    @NumberFormat
+    private String phoneNumber;
+
+    @NotBlank(message = "Website required")
+    private String website;
+
+    private String emergency;
+
+    private String status;
+
+    @OneToOne
+    private Clinic clinic;
+
+    @OneToOne
+    private User user;
+
+    @CreationTimestamp
+    private Date createdTimestamp;
+
+    public Request() {};
+
+    public Request(String name, String address, String city, String state, String zip, String phoneNumber, String website, String emergency, String status, User user) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.website = website;
+        this.emergency = emergency;
+        this.status = status;
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Clinic getClinic() {
+        return clinic;
+    }
+
+    public void setClinic(Clinic clinic) {
+        this.clinic = clinic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getEmergency() {
+        return emergency;
+    }
+
+    public void setEmergency(String emergency) {
+        this.emergency = emergency;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreatedTimestamp() {
+        return createdTimestamp;
+    }
+
+    public void setCreatedTimestamp(Date createdTimestamp) {
+        this.createdTimestamp = createdTimestamp;
+    }
+}
