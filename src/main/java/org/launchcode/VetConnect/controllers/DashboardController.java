9beb9e7a9df75ee491f152dc8f3200877468f681
Kelly Jones <kellymj6@gmail.com>
2022-02-13 23:46:05 -0500

    Added css for table cells to shrink or expand

---
+++ src/main/java/org/launchcode/VetConnect/controllers/DashboardController.java
@@ -0,0 +1,39 @@
+package org.launchcode.VetConnect.controllers;
+
+import org.launchcode.VetConnect.models.User;
+import org.launchcode.VetConnect.models.data.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class DashboardController extends VetConnectController{
+
+    @Autowired
+    RequestRepository requestRepository;
+
+
+    @GetMapping(value="dashboard-pet-owner")
+    public String displayDashboardPetOwner(Model model, HttpServletRequest request) {
+        User this_user = getUserFromSession(request.getSession(false));
+        if (!this_user.getUserType().equals("petOwner")) {
+            return "redirect:error";
+        }
+        model.addAttribute("requests", this_user.getRequests());
+        return "dashboard-pet-owner";
+    }
+
+    @GetMapping(value="dashboard-vet")
+    public String displayDashboardVet(Model model, HttpServletRequest request) {
+        User this_user = getUserFromSession(request.getSession(false));
+        if (!this_user.getUserType().equals("vet")) {
+            return "redirect:error";
+        }
+        model.addAttribute("requests", this_user.getRequests());
+        return "dashboard-vet";
+    }
+
+}
