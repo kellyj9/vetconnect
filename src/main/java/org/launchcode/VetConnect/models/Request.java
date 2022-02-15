e83ebd18d5ea383611abc326585a2bd0d7ee7a05
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-02-15 16:20:11 -0500

    improved phone number validation

--- src/main/java/org/launchcode/VetConnect/models/Request.java
+++ src/main/java/org/launchcode/VetConnect/models/Request.java
@@ -28,11 +28,12 @@ public class Request extends AbstractEntity {
 
     @NotBlank(message = "Zip required")
     @NumberFormat
-    @Size(min=6,max=6, message="Must be a valid Zip Code")
+    @Size(min=5,max=5, message="Must be a valid Zip Code")
     private String zip;
 
     @NotBlank(message = "Phone Number required")
-    @Pattern(regexp="(^$|[0-9]{10})", message="Must be a valid phone number")
+    @Pattern(regexp="^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$",
+            message="Must be a valid phone number")
     private String phoneNumber;
 
     @NotBlank(message = "Website required")
