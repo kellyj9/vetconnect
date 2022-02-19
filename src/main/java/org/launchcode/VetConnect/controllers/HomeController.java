d8eb5f5282d4dbf543cbedec56b53f5fac5ad314
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-02-18 20:22:49 -0500

    moved review to clinic profile

--- src/main/java/org/launchcode/VetConnect/controllers/HomeController.java
+++ src/main/java/org/launchcode/VetConnect/controllers/HomeController.java
@@ -3,26 +3,33 @@ package org.launchcode.VetConnect.controllers;
 
 import org.launchcode.VetConnect.models.ClinicData;
 import org.launchcode.VetConnect.models.Review;
+import org.launchcode.VetConnect.models.User;
 import org.launchcode.VetConnect.models.data.ClinicRepository;
 import org.launchcode.VetConnect.models.Clinic;
 import org.launchcode.VetConnect.models.data.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalDouble;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.*;
 
 @Controller
 public class HomeController extends VetConnectController {
 
     @Autowired
     private ClinicRepository clinicRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     @GetMapping(value="")
     public String displayIndex() {
@@ -54,18 +61,55 @@ public class HomeController extends VetConnectController {
     }
 
     @GetMapping("clinic-profile")
-    public String displayClinicProfile(@RequestParam Long clinicId, Model model)
+    public String displayClinicProfile(@RequestParam Long clinicId, Model model, HttpServletRequest request)
     {
         Optional<Clinic> clinic = clinicRepository.findById(clinicId);
+        User user = getUserFromSession(request.getSession(false));
 
         if(clinic.isPresent()) {
-            OptionalDouble average = clinic.get().getReviews().stream().mapToDouble(a -> a.getReviewRating()).average();
+            List<Review> reviews = clinic.get().getReviews();
+
+            if(!(reviews.isEmpty())) {
+                DecimalFormat df = new DecimalFormat("#.##");
+                df.setRoundingMode(RoundingMode.CEILING);
+
+                OptionalDouble average = reviews.stream().mapToDouble(a -> a.getReviewRating()).average();
+                model.addAttribute("average", df.format(average.getAsDouble()));
+
+            }
+
+            if(!(user == null)) {
+                Review review = reviewRepository.findByUserIdAndClinicId(user.getId(), clinicId);
+
+                if(review == null) {
+                    model.addAttribute(new Review());
+                }
+            }
 
             model.addAttribute("clinic", clinic.get());
-            model.addAttribute("average", average.getAsDouble());
         }
 
         return "clinic-profile";
+    }
+
+    @PostMapping("clinic-profile")
+    public String addAReviewRequest(@ModelAttribute @Valid Review newReview, @RequestParam Long clinicId, Model model, Errors errors, HttpServletRequest request) {
+        if(errors.hasErrors()) {
+            return "clinic-profile";
+        }
+
+        Optional<Clinic> clinic = clinicRepository.findById(clinicId);
+
+        if(clinic.isPresent()) {
+            User user = getUserFromSession(request.getSession(false));
+            newReview.setUser(user);
+            newReview.setClinic(clinic.get());
+        }
+
+        reviewRepository.save(newReview);
+
+
+        return "redirect:clinic-profile?clinicId=" + clinicId;
     }
 
 
