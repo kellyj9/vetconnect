aafc3cd1fe2cc9c7eef6c37c1bd1edff97fb36aa
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-02-18 23:28:46 -0500

    initial claim entity

---
+++ src/main/java/org/launchcode/VetConnect/models/Claim.java
@@ -0,0 +1,52 @@
+package org.launchcode.VetConnect.models;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Claim extends AbstractEntity{
+
+    @OneToOne
+    private Clinic clinic;
+
+    @ManyToOne
+    private User user;
+
+    @CreationTimestamp
+    private Date createdTimestamp;
+
+    public Claim() {}
+
+    public Claim(Clinic clinic, User user) {
+        this.clinic = clinic;
+        this.user = user;
+    }
+
+    public Date getCreatedTimestamp() {
+        return createdTimestamp;
+    }
+
+    public void setCreatedTimestamp(Date createdTimestamp) {
+        this.createdTimestamp = createdTimestamp;
+    }
+
+    public Clinic getClinic() {
+        return clinic;
+    }
+
+    public void setClinic(Clinic clinic) {
+        this.clinic = clinic;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
