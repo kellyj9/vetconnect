0d4b4b439bc2bd13f973bb32aa823cc90637fb4b
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-02-18 20:23:33 -0500

    added findbyuserandclinic method

--- src/main/java/org/launchcode/VetConnect/models/data/ReviewRepository.java
+++ src/main/java/org/launchcode/VetConnect/models/data/ReviewRepository.java
@@ -4,4 +4,6 @@ import org.launchcode.VetConnect.models.Review;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReviewRepository extends CrudRepository<Review, Long>{
+
+    Review findByUserIdAndClinicId (Long user_id, Long clinic_id);
 }
