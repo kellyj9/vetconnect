4eb5c9f1e93b2fefd9ad9fafcb429bf3d926d7da
nburbinski <32502858+nburbinski@users.noreply.github.com>
2022-02-18 20:23:20 -0500

    added validation

--- src/main/java/org/launchcode/VetConnect/models/Review.java
+++ src/main/java/org/launchcode/VetConnect/models/Review.java
@@ -4,13 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Review extends AbstractEntity{
 
-    @NotBlank
     private String reviewText;
 
+    @NotNull
     private Integer reviewRating;
 
     @ManyToOne
